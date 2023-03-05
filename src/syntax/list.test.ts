99e47fb7979ee54b850bce44504bc8747f78c6bf
Ally Murray <allymurray88@gmail.com>
2023-03-05 09:57:36 +0000

    feat: :sparkles: add list syntax

---
+++ src/syntax/list.test.ts
@@ -0,0 +1,62 @@
+import { orderedList, taskList, unorderedList } from './list.js';
+
+describe('list', () => {
+  it('should return the a list of numbered items', () => {
+    expect(orderedList(['First Item', 'Second Item'])).toBe(
+      ['1. First Item', '2. Second Item'].join('\n')
+    );
+  });
+
+  it('should return an unordered list of items prefixed with *', () => {
+    expect(unorderedList(['First Item', 'Second Item'])).toBe(
+      ['* First Item', '* Second Item'].join('\n')
+    );
+  });
+
+  it('should mark the task as complete when complete is true', () => {
+    expect(
+      taskList([
+        {
+          description: 'Task Item 1',
+          complete: true,
+        },
+      ])
+    ).toBe('- [x] Task Item 1');
+  });
+
+  it('should not mark the task as complete when complete is false', () => {
+    expect(
+      taskList([
+        {
+          description: 'Task Item 1',
+          complete: true,
+        },
+      ])
+    ).toBe('- [x] Task Item 1');
+  });
+
+  it('should return a new line separated list of items', () => {
+    expect(
+      taskList([
+        {
+          description: 'Task Item 1',
+          complete: true,
+        },
+        {
+          description: 'Task Item 2',
+        },
+      ])
+    ).toBe(['- [x] Task Item 1', '- [ ] Task Item 2'].join('\n'));
+  });
+
+  it('should escape the task list description if it begins with a parenthesis', () => {
+    expect(
+      taskList([
+        {
+          description: '(Important) An important task',
+          complete: true,
+        },
+      ])
+    ).toBe('- [x] \\(Important) An important task');
+  });
+});
---
+++ src/syntax/list.ts
@@ -0,0 +1,21 @@
+type Task = { description: string; complete?: boolean };
+
+export const orderedList = (items: Array<string>) => {
+  return items.map((item, index) => `${index + 1}. ${item}`).join('\n');
+};
+
+export const unorderedList = (items: Array<string>) => {
+  return items.map((item) => `* ${item}`).join('\n');
+};
+
+export const taskList = (tasks: Array<Task>) => {
+  return tasks
+    .map((task) => {
+      const completeMarker = task.complete ? 'x' : ' ';
+      const description = task.description.startsWith('(')
+        ? `\\${task.description}`
+        : task.description;
+      return `- [${completeMarker}] ${description}`;
+    })
+    .join('\n');
+};
