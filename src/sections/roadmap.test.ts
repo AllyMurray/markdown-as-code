078840ce99f5bc8700ad4e912234692b85e28179
Ally Murray <allymurray88@gmail.com>
2023-03-07 19:08:49 +0000

    feat: :sparkles: add roadmap section

---
+++ src/sections/roadmap.test.ts
@@ -0,0 +1,24 @@
+import { Roadmap } from './roadmap.js';
+
+describe('Roadmap', () => {
+  it('should return title only when no items are added', () => {
+    const section = new Roadmap();
+    expect(section.synthesize()).toBe('## Roadmap\n\n');
+  });
+
+  it('should return correct syntax when one item is added', () => {
+    const section = new Roadmap().add({ text: 'Item 1' });
+    expect(section.synthesize()).toBe(
+      ['## Roadmap', '', '* Item 1'].join('\n')
+    );
+  });
+
+  it('should return correct syntax when multiple items is added', () => {
+    const section = new Roadmap()
+      .add({ text: 'Item 1' })
+      .add({ text: 'Item 2' });
+    expect(section.synthesize()).toBe(
+      ['## Roadmap', '', '* Item 1', '', '* Item 2'].join('\n')
+    );
+  });
+});
---
+++ src/sections/roadmap.ts
@@ -0,0 +1,15 @@
+import { ListSection } from './list-section.js';
+
+interface RoadmapItem {
+  text: string;
+}
+
+export class Roadmap extends ListSection<RoadmapItem> {
+  constructor(title?: string) {
+    super(title ?? 'Roadmap', 'Unordered');
+  }
+
+  protected itemMapper(item: RoadmapItem): string {
+    return item.text;
+  }
+}
