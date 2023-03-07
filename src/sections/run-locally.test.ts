24fcb655f7198351b2b8080ddb4f364580679e21
Ally Murray <allymurray88@gmail.com>
2023-03-07 18:47:25 +0000

    feat: :sparkles: add run locally section

---
+++ src/sections/run-locally.test.ts
@@ -0,0 +1,55 @@
+import { RunLocally } from './run-locally.js';
+
+describe('Run Locally', () => {
+  it('should return title only when no items are added', () => {
+    const section = new RunLocally();
+    expect(section.synthesize()).toBe('## Run Locally\n\n');
+  });
+
+  it('should return correct syntax when one item is added', () => {
+    const section = new RunLocally().add({
+      command: 'npm t',
+      description: 'Run the tests',
+    });
+    expect(section.synthesize()).toBe(
+      [
+        '## Run Locally',
+        '',
+        '* Run the tests',
+        '',
+        '```bash',
+        'npm t',
+        '```',
+      ].join('\n')
+    );
+  });
+
+  it('should return correct syntax when multiple items is added', () => {
+    const section = new RunLocally()
+      .add({
+        command: 'npm t',
+        description: 'Run the tests',
+      })
+      .add({
+        command: 'npm start',
+        description: 'Start the app',
+      });
+    expect(section.synthesize()).toBe(
+      [
+        '## Run Locally',
+        '',
+        '* Run the tests',
+        '',
+        '```bash',
+        'npm t',
+        '```',
+        '',
+        '* Start the app',
+        '',
+        '```bash',
+        'npm start',
+        '```',
+      ].join('\n')
+    );
+  });
+});
---
+++ src/sections/run-locally.ts
@@ -0,0 +1,16 @@
+import { ListSection } from './list-section.js';
+
+interface RunLocalStep {
+  description: string;
+  command: string;
+}
+
+export class RunLocally extends ListSection<RunLocalStep> {
+  constructor(title?: string) {
+    super(title ?? 'Run Locally', 'Unordered');
+  }
+
+  protected itemMapper(step: RunLocalStep): string {
+    return [step.description, '', '```bash', step.command, '```'].join('\n');
+  }
+}
